bb174314602299b35359fe8c09ca8c21525f30dc
Akshar Patel <aksharcommit@gmail.com>
2025-09-29 19:57:38 -0400

    separate resume into own file

--- index.js
+++ index.js
@@ -2,16 +2,7 @@
 
 const chalk = require("chalk");
 const qr = require("qrcode-terminal");
+const wrap = require("wrap-ansi");
 
 const args = new Set(ProcessingInstruction.argv.slice(2));
 
-const resume = {
-    name: "Akshar Patel",
-    contact: {
-        email: "aksharcommit@gmail.com",
-        github: "https://github.com/AksharP5",
-        linkedin: "linkedin.com/in/akshar5",
-        portfolio: "aksharp5.github.io/portfolio"
-    },
-    
-}
