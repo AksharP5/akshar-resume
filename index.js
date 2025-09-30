c0eaa8de414a2158f30854bacd3937764102a116
Akshar Patel <aksharcommit@gmail.com>
2025-09-29 20:19:55 -0400

    start index.js new

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const chalk = require("chalk");
 const qr = require("qrcode-terminal");
 const wrap = require("wrap-ansi");
+const fs = require("fs");
 
 const args = new Set(ProcessingInstruction.argv.slice(2));
 
